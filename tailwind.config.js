980fffb22b56fb48bb015a3f94b88a2240badb73
Carlos Silva <miguel21.melo21@gmail.com>
2023-04-13 19:50:38 -0300

    feat: challenge completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        gray: {
+          50: "#F8FAFC",
+          200: "#E2E8F0",
+          300: "#CBD5E1",
+          600: "#475569",
+          800: "#1E293B",
+          900: "#0F172A",
+        },
+        purple: {
+          100: "#996DFF",
+          200: "#7C3AED",
+        },
+        orange: {
+          500: "#FBA94C",
+        },
       },
     },
   },
   plugins: [],
-}
+};
